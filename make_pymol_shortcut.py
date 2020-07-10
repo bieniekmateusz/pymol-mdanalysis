647149cf3082afcaffc92b27bac1edd3b938c3ee
bieniekmat <bieniekmat@gmail.com>
2020-07-10 19:48:55 +0100

    A version that works on linux

--- make_pymol_shortcut.py
+++ make_pymol_shortcut.py
@@ -4,6 +4,7 @@ Generate a shortcut for the application.
 """
 
 import sys
+import os
 
 import pymol
 
@@ -14,5 +15,10 @@ pymol_file = pymol.__file__
 # create a shortcut to the executable file, using the right environment
 from pyshortcuts import make_shortcut
 
-make_shortcut(sys.executable + ' ' + pymol_file, name='PymolMda',
-              icon='data/pymol/icons/icon2.svg', terminal=False)
+if os.name == 'nt':
+    # todo: only tested on conda base env
+    make_shortcut(sys.executable + ' ' + pymol_file, name='PymolMda',
+                  icon='data/pymol/icons/icon2.svg', terminal=False)
+else:
+    make_shortcut(pymol_file, name='PymolMda',
+                  icon='data/pymol/icons/icon2.svg', terminal=False)
