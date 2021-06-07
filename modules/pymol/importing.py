89f150aeb89d10d1050457e05611908efb429222
bieniekmat <bieniekmat@gmail.com>
2021-06-07 09:57:37 +0100

    Allow users to specify ie selection "name CA"

--- modules/pymol/importing.py
+++ modules/pymol/importing.py
@@ -585,7 +585,7 @@ SEE ALSO
         return None
 
 
-    def mda_rmsf(label, _self=cmd):
+    def mda_rmsf(label, selection = "backbone", _self=cmd):
         '''
         DESCRIPTION
             "mda_rmsf" computes RMSF for the label, and visualises it.
@@ -595,7 +595,6 @@ SEE ALSO
         PYMOL API
             cmd.mda_rmsf(label)
         '''
-        selection = "backbone"
         pylustrator = False,
 
         from MDAnalysis.analysis.rms import RMSF
